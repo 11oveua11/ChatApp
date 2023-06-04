d2ca178ca9313ccdf1b5d4a9a813fa728ec5fd66
Adm <khost@bk.ru>
2023-06-04 17:56:14 +0300

    Resize works, layout is good, tabs added (need to add tabs autocreation. ..And still needs emoji list.

--- main.py
+++ main.py
@@ -13,28 +13,39 @@ class DlgMain(QDialog):
         self.resize(400, 400)
         self.setWindowIcon(QIcon('icon1.png'))
 
-        self.message = AdvMessage()
-        self.msg_as_txt = ""
+        # self.message = AdvMessage()
+        self.msg_as_txt_0 = ""
 
-        # self.frame_bottom = QFrame(self)
-        # self.frame_bottom.setFrameShape(QFrame.StyledPanel)
+        self.txt_Browser_0 = QTextBrowser(self)
+        #self.txtBrowser.resize(100, 80)
+        self.txt_Browser_0.setText('')
+        # self.txtBrowser.setAlignment(Qt.AlignBottom | Qt.AlignLeft)
 
-        self.txtBrowser = QTextBrowser(self)
-        self.txtBrowser.resize(400, 300)
-        self.txtBrowser.setText('')
-        self.txtBrowser.setAlignment(Qt.AlignBottom | Qt.AlignLeft)
-
-        self.txtEdit = QTextEdit(self)
+        self.txt_Edit_0 = QTextEdit(self)
         # self.txtEdit.move(0, 300)
-        self.txtEdit.resize(400, 80)
-        self.txtEdit.textChanged.connect(self.txtEdit_changed)
+        #self.txtEdit.resize(100, 50)
+        self.txt_Edit_0.textChanged.connect(self.txtEdit_changed)
 
-        self.splitter = QSplitter(Qt.Vertical)
-        self.splitter.addWidget(self.txtBrowser)
-        self.splitter.addWidget(self.txtEdit)
+        self.tabWidget = QTabWidget(self)
+        self.tab_0 = QWidget()
+        self.vbox_tab_layout_0 = QVBoxLayout(self.tab_0)
+        # self.tab_0.setStyleSheet('background-color: red')
+        self.splitter_0 = QSplitter(Qt.Vertical)
+        self.splitter_0.addWidget(self.txt_Browser_0)
+        self.splitter_0.addWidget(self.txt_Edit_0)
+        self.splitter_0.setChildrenCollapsible(False)
+        self.splitter_0.setSizes([300, ])
+        self.vbox_tab_layout_0.addWidget(self.splitter_0)
+        # self.tab_0.resize(self, [350, 300])
+
+        # self.splitter_0.setSizes([240, 100])
+
 
         self.vbox_main_layout = QVBoxLayout(self)
-        self.vbox_main_layout.addWidget(self.splitter)
+        self.vbox_main_layout.addWidget(self.tabWidget)
+        self.tabWidget.addTab(self.tab_0, "Tab 0")
+
+
 
         self.hbox_layout = QHBoxLayout(self)
 
@@ -50,24 +61,49 @@ class DlgMain(QDialog):
         self.btnAdditions.clicked.connect(self.btnAdditions_clicked)
         self.btnAdditions.setIcon(QIcon('emoji-icon.png'))
 
+        self.btnTemp = QPushButton(self)
+        self.btnTemp.clicked.connect(self.add_new_tab)
+
         self.hbox_layout.addWidget(self.btnSend)
         self.hbox_layout.addWidget(self.btnAdditions)
-
+        self.hbox_layout.addWidget(self.btnTemp)
 
         self.vbox_main_layout.addLayout(self.hbox_layout)
 
 
+
+    def add_new_tab(self):
+
+        self.msg_as_txt_0 = ""
+
+        self.txt_Browser_0 = QTextBrowser(self)
+        # self.txtBrowser.resize(400, 300)
+        self.txt_Browser_0.setText('')
+        self.txt_Browser_0.setAlignment(Qt.AlignBottom | Qt.AlignLeft)
+
+        self.txt_Edit_0 = QTextEdit(self)
+        # self.txtEdit.move(0, 300)
+        # self.txtEdit.resize(400, 80)
+        self.txt_Edit_0.textChanged.connect(self.txtEdit_changed)
+
+        self.splitter_0 = QSplitter(Qt.Vertical)
+        self.splitter_0.addWidget(self.txt_Browser_0)
+        self.splitter_0.addWidget(self.txt_Edit_0)
+
+        self.vbox_main_layout = QVBoxLayout(self)
+        self.vbox_main_layout.addWidget(self.splitter_0)
+
     def btnSend_clicked(self):
         # self.hist_text = self.hist_text + '\n\n' + self.txtEdit.toPlainText()
 
-        self.txtBrowser.append(emoji.emojize(self.msg_as_txt))
-        self.txtEdit.clear()
-        self.txtEdit.setFocus()
+        self.txt_Browser_0.append(emoji.emojize(self.msg_as_txt_0))
+        self.txt_Edit_0.clear()
+        self.txt_Edit_0.setFocus()
 
     def btnAdditions_clicked(self):
-        self.msg_as_txt = self.txtEdit.toPlainText() + ':thumbs_up:'
-        self.txtEdit.setText(emoji.emojize(self.msg_as_txt))
-        self.txtEdit.setFocus()
+        self.msg_as_txt_0 = self.txt_Edit_0.toPlainText() + ':thumbs_up:'
+        self.txt_Edit_0.setText(emoji.emojize(self.msg_as_txt_0))
+        self.txt_Edit_0.setFocus()
 
 
 
@@ -75,10 +111,10 @@ class DlgMain(QDialog):
         pass
 
     def txtEdit_changed(self):
-        self.msg_as_txt = self.txtEdit.toPlainText()
-        cursor = self.txtEdit.textCursor()
+        self.msg_as_txt_0 = self.txt_Edit_0.toPlainText()
+        cursor = self.txt_Edit_0.textCursor()
         cursor.movePosition(QTextCursor.End)
-        self.txtEdit.setTextCursor(cursor)
+        self.txt_Edit_0.setTextCursor(cursor)
 
 
 
