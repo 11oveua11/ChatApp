7819a8db452f1aa55a2e882bd9a012cc4eed8708
w-Adm <khost@bk.ru>
2023-06-11 19:34:03 +0300

    + 1. Cuted margins for layouts, + 2. Window resize works, + 3. Tabs added, + 4. Tabs autocreation added, + 4.1. Autocreation realized in Tabs class. + 4.2. Need to init main tab in class. - And still needs emoji list.++ - Need message class for complecs message objects, to save chat history in DB.

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ class DlgMain(QDialog):
         self.setWindowIcon(QIcon('icon1.png'))
 
         # self.message = AdvMessage()
-        self.msg_as_txt_0 = ""
+        # self.msg_as_txt_0 = ""
         self.tab_widget = QTabWidget(self)
         self.cur_tab = 0                    # Index of active tab widget
         self.tab_list = []                  # List of tabs(Qwidget) for tab_widget(QTabWidget)
-
+        self.emoji_list = ["&#128512;", "&#128513;", "&#128514;", "&#128515;"]
 
         self.tab_list.append(Tab())
         self.tab_widget.currentChanged.connect(self.tab_widget_changed)
@@ -50,6 +50,12 @@ class DlgMain(QDialog):
         self.emoji_qwidget.setWindowFlags(Qt.Popup)
         self.emoji_qwidget.resize(200, 200)
 
+        self.emoji_txt_browser = QTextBrowser(self.emoji_qwidget)
+        self.emoji_txt_browser.resize(200, 200)
+        self.emoji_txt_browser.setHtml(self.emoji_to_html(self.emoji_list))
+        self.emoji_txt_browser.setOpenLinks(False)
+        self.emoji_txt_browser.anchorClicked.connect(self.emoji_link_clicked)
+
         # emoji_txt_browser.setHidden(True)
 
         self.btn_temp = QPushButton("add new tab", self)
@@ -76,20 +82,18 @@ class DlgMain(QDialog):
         self.tab_list[self.cur_tab].txt_edit.setFocus()
 
     def btnAdditions_clicked(self):
-        pass
+
         print(self.pos())
         print(self.btn_additions.pos())
         self.emoji_qwidget.move(self.btn_additions.pos() + self.pos() + QPoint(0, 30))
-        self.emoji_txt_browser = QTextBrowser(self.emoji_qwidget)
-        self.emoji_txt_browser.resize(200, 200)
         print(self.emoji_qwidget.pos())
         self.emoji_qwidget.show()
 
-
-
-        # self.tab_list[self.cur_tab].msg_as_txt = self.tab_list[self.cur_tab].txt_edit.toPlainText() + ':thumbs_up:'
-        # self.tab_list[self.cur_tab].txt_edit.setText(emoji.emojize(self.tab_list[self.cur_tab].msg_as_txt))
-        # self.tab_list[self.cur_tab].txt_edit.setFocus()
+    def emoji_link_clicked(self, url):
+        print(url)
+        self.tab_list[self.cur_tab].msg_as_txt = self.tab_list[self.cur_tab].txt_edit.toPlainText() + ':thumbs_up:'
+        self.tab_list[self.cur_tab].txt_edit.setText(emoji.emojize(self.tab_list[self.cur_tab].msg_as_txt))
+        self.tab_list[self.cur_tab].txt_edit.setFocus()
 
 
 
@@ -100,13 +104,18 @@ class DlgMain(QDialog):
         self.cur_tab = self.tab_widget.currentIndex()
 
     def emoji_to_html(self, emoji_list):
-        main_txt = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
-<html><head><meta name="qrichtext" content="1" /><style type="text/css">
-p, li { white-space: pre-wrap; }
-</style></head><body style=" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;">
-"""
+#         main_txt = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
+# <html><head><meta name="qrichtext" content="1" /><style type="text/css">
+# p, li { white-space: pre-wrap; }
+# </style></head><body style=" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;">
+# """
+        main_txt = ''
+        a1 = """<a href="""
+        a2 = """>"""
+        a3 = """</a>"""
         for em in emoji_list:
-            main_txt = main_txt + """<a href="http://foo>Bar</a>""" + """</a>"""
+            main_txt = main_txt + (a1 + str(emoji_list.index(em)) + a2 + em + a3)
+        return main_txt
 
 
 
